bcd526c59b483277a31ebc00ad2c8e6b14a6bcc7
JeniLam <jenilam639@gmail.com>
2020-11-21 23:57:28 -0600

    starts app code

--- Mission_To_Mars/app.py
+++ Mission_To_Mars/app.py
@@ -1,4 +1,11 @@
 from flask import Flask, render_template, redirect
+from flask_pymongo import PyMongo
 import scrape_mars
 
 app = Flask(__name__)
+
+# Use PyMongo to establish Mongo connection
+mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app
+
+@app.route("/")
+def home():
